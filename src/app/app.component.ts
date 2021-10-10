bcd35702facf832f0c8c774e8616c8351ca68b66
Sreekanth Mandala <smandala@ariqt.com>
2021-10-10 15:02:43 +0530

    into sreekanth branch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'authentication';
+  title = 'customepipe';  
+  arrList: any = [];  
+  constructor() {}  
+  ngOnInit() {
+    this.arrList = [  
+      { id: 1, name: 'Name1', value: null, status: 'pending' },  
+      { id: 2, name: 'Name2', value: null, status: 'pending' },  
+      { id: 3, name: 'Name3', value: 'View', status: 'pending' },  
+    ];
+  }
 }
